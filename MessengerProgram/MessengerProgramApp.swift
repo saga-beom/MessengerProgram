475cdf984ae89faaf5e33d968906c79021cf86ad
saga-beom <hyobeom003@gmail.com>
2023-01-29 02:24:17 +0900

    Initial Commit

---
+++ MessengerProgram/MessengerProgramApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerProgramApp.swift
+//  MessengerProgram
+//
+//  Created by hyobeom seo on 2023/01/29.
+//
+
+import SwiftUI
+
+@main
+struct MessengerProgramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
